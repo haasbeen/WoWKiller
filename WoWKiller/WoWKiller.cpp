3f9047bbd9833309b66b22c22975ae4f89a36805
Kyle <runolfsonkyle@yahoo.com>
2023-02-27 20:14:38 -0800

    Here we goooo

---
+++ WoWKiller/WoWKiller.cpp
@@ -0,0 +1,58 @@
+/*
+
+a fantasy style rpg dungeon crawler
+
+some classes:
+- grid (maybe)
+- mobs (maybe different types like: slime, goblin, ogre, dragon (rare))
+- weapons (2nd phase)
+- player
+- classes (2nd phase)
+
+attributes of a mob:
+- crit chance
+- miss chance
+- max health
+- current health
+- base damage
+
+attributes of a player:
+- crit chance
+- miss chance
+- max health
+- current health
+- base damage
+
+grid:
+- 10x10 board
+- up, down, left, right
+- walls and boundaries
+- x = wall
+
+x x x x x x x x   x
+x   x x   x x     x
+x         x       x
+x x   x       x   x
+x x   x x x   x x x
+x     x       x   x
+x   x x x x       x
+x     x   x x x   x
+x x       x       x
+x     x       x   x
+x   x x x x x x x x
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main()
+{
+
+}
+
+
